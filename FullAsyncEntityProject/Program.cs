12bbdbf4ad3a94a961f8aa286b62a3fea76b6bab
camalzadeelvin <camalzadeelvin@STHQ011A-01>
2026-04-06 11:16:07 +0400

    Add project files.

---
+++ FullAsyncEntityProject/Program.cs
@@ -0,0 +1,40 @@
+﻿using FullAsyncEntityProject.DataAccess;
+using FullAsyncEntityProject.DataAccess.Concrete.EntityFramework;
+using FullAsyncEntityProject.Services.Concrete;
+
+namespace FullAsyncEntityProject
+{
+    public class Program
+    {
+        static async void Start()
+        {
+            using (var context = new StepDBContext())
+            {
+                var productDal = new EFProductDal(context);
+                var productService = new ProductService(productDal);
+
+                var products = await productService.GetListAsync();
+                foreach (var p in products)
+                {
+                    Console.WriteLine($"ID : {p.Id}");
+                    Console.WriteLine($"Name : {p.Name}");
+                    Console.WriteLine($"Description : {p.Description}");
+                    Console.WriteLine($"Price : {p.Price}$");
+                    Console.WriteLine($"Quantity : {p.Quantity}");
+                }
+
+            }
+        }
+
+        static async Task Menu()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Start();
+            Console.ReadLine();
+        }
+    }
+}
